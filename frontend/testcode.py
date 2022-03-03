78295e78dae4c780696ea9ec42c8ef736b1a1775
KalStormBYU <kalstorm@Predator343.localdomain>
2022-03-03 09:54:52 -0700

    add AWS auth

--- frontend/testcode.py
+++ frontend/testcode.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-#import requests, 
+import requests
 import argparse
 import cmd2
+from aws_requests_auth.aws_auth import AWSRequestsAuth
 
 loggedIn = False
 
@@ -29,12 +30,19 @@ class cvetracker(cmd2.Cmd):
     #@cmd2.with_argparser(role_parser)
     def do_role(self, args):
         """Print the role of the user"""
-        amiLogged()
+        #amiLogged()
         if loggedIn == False:
-            self.poutput("Sorry, can't access the database")
+            self.poutput("Sorry, can't access the database. You are not logged in.")
         else:
             self.poutput("Welcome to the database!")
             self.poutput("What would you like to do?")
+
+    def do_login(self, args):
+        """Login to AWS account"""
+        print("Attempting to log you in")
+        key = input("Please enter your key: ")
+        secret = input("Please enter your secret key: ")
+        awsLogin(key, secret)
 
 def amiLogged():
     global loggedIn
@@ -43,6 +51,18 @@ def amiLogged():
         loggedIn = True
     elif resp == 'y':
         loggedIn = False
+
+def awsLogin(key, secret):
+    global loggedIn
+    auth = AWSRequestsAuth(aws_access_key=key,
+            aws_secret_access_key=secret,
+            aws_host='to36jhw9b1.execute-api.us-west-2.amazonaws.com',
+            aws_region='us-west-2',
+            aws_service='execute-api')
+    headers = {'testing': '123abc'}
+    response = requests.get('https://to36jhw9b1.execute-api.us-west-2.amazonaws.com/default/all_computers_apps_vulns', auth=auth, headers=headers)
+    if response.status_code == requests.codes.ok:
+        loggedIn = True
 
 
 if __name__ == '__main__':
