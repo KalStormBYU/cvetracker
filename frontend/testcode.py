d3bc27b78b4b3f6cffbd7d22a59ec3fe1bb3ab0d
KalStormBYU <todd.r.andrewsen@gmail.com>
2022-03-02 22:58:58 -0700

    update

--- frontend/testcode.py
+++ frontend/testcode.py
@@ -4,14 +4,6 @@
 import argparse
 import cmd2
 
-"""
-parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--role", help="display role of current user", action='store_true')
-args = parser.parse_args()
-if args.role:
-    print("Role now displays properly")
-"""
-
 loggedIn = False
 
 class cvetracker(cmd2.Cmd):
