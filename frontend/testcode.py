61abd881febf0954dda2ada4ecd7e542294bcd02
KalStormBYU <kalstorm@Predator343.localdomain>
2022-03-14 23:36:46 -0600

    add more Analyst functionality

--- frontend/testcode.py
+++ frontend/testcode.py
@@ -67,30 +67,33 @@ class cvetracker(Cmd):
 
     def do_role(self, args):
         """Print the role of the user"""
-        #amiLogged()
         if loggedIn == False:
             self.poutput("Sorry, can't access the database. You are not logged in.")
         else:
-            self.poutput("Welcome to the database!")
             self.poutput(f'Your Role is {role.upper()}')
 
     def do_login(self, args):
         """Login to AWS account"""
-        print("Attempting to log you in")
-        key = input("Please enter your key: ")
-        secret = input("Please enter your secret key: ")
-        awsLogin(key, secret)
+        if( not loggedIn):
+            print("Attempting to log you in")
+            key = input("Please enter your key: ")
+            secret = input("Please enter your secret key: ")
+            awsLogin(key, secret)
+        else:
+            print("You're already logged in.")
 
     def do_logout(self, args):
         """Logout of AWS account"""
         global loggedIn
         global auth
         global role
-        print("Logging you out now.")
-        loggedIn = False
-        auth = 0
-        role = ""
-        print("Successfully logged you out!")
+        if(loggedIn):
+            loggedIn = False
+            auth = 0
+            role = ""
+            print("Successfully logged you out!")
+        else:
+            print("You aren't logged in.")
 
 
     @cmd2.with_argparser(list_parser)
@@ -100,7 +103,11 @@ class cvetracker(Cmd):
         if func is not None:
             func(self, args)
         else:
-            self.do_help('list')
+            if(loggedIn):
+                self.do_help('list')
+            else:
+                self.poutput("Sorry, can't access the database. You are not logged in.")
+
 
     def list_computer(self, args):
         if(loggedIn):
@@ -139,13 +146,14 @@ class cvetracker(Cmd):
                 self.poutput('\n')
                 b_id = input("Which Business Unit would you like to view? (ID NUMBER): ")
                 try:
-                    data = {'id': int(b_id)}
+                    #data = {'id': int(b_id)}
+                    data = {'id': float(b_id)}
                     response = requests.get(method + url_sysadmin2 + '/sysadmin2_all_computers_in_one_bu', auth=auth, json=data)
                     tabledata = response.json()
                     l1,l2 = len(tabledata), len(tabledata[0])
                     df = pd.DataFrame(tabledata, index=['']*l1, columns=['']*l2)
                     df.set_axis(['COMPUTER NAME','COMPUTER ID'],axis=1,inplace=True)
-                    self.poutput('Here are the computers you requested:')
+                    self.poutput(f'Here are the computers you requested:')
                     self.poutput(df.head())
                     self.poutput('\n')
                 except ValueError:
@@ -155,16 +163,22 @@ class cvetracker(Cmd):
                 except IndexError:
                     print("The business unit you requested is empty.")
             elif role == 'analyst':
-                data = {'values': args.severity}
-                response = requests.get(method + url_analyst + '/analyst_list_all_info_by_severity', auth=auth, json=data)
-                self.poutput('Here is your data')
-                tabledata = response.json()
-                l1,l2 = len(tabledata), len(tabledata[0])
-                df = pd.DataFrame(tabledata, index=['']*l1, columns=['']*l2)
-                df.set_axis(['CVE', 'SEVERITY', 'COMPUTER NAME', 'OS', 'OS_VERSION','BUSINESS UNIT'],axis=1,inplace=True)
-                self.poutput('Here are the computers you requested:')
-                self.poutput(df.head())
-                self.poutput('\n')
+                sev = input("Please enter the severity level for CVEs on Computers in the organization (1-10): ")
+                try:
+                    data = {'values': int(sev)}
+                    response = requests.get(method + url_analyst + '/analyst_list_all_info_by_severity', auth=auth, json=data)
+                    tabledata = response.json()
+                    l1,l2 = len(tabledata), len(tabledata[0])
+                    df = pd.DataFrame(tabledata, index=['']*l1, columns=['']*l2)
+                    df.set_axis(['CVE', 'SEVERITY', 'COMPUTER NAME', 'OS', 'OS_VERSION','BUSINESS UNIT'],axis=1,inplace=True)
+                    self.poutput('Here are the computers you requested:')
+                    self.poutput(df.head())
+                    self.poutput('\n')
+                except ValueError:
+                    print("Please enter a valid integer")
+                except IndexError:
+                    print(f"There are no CVEs with severities above {sev}")
+
         else:
             self.poutput("You must log in to view this data.")
     parser_computer.set_defaults(func=list_computer)
@@ -190,15 +204,22 @@ class cvetracker(Cmd):
                 self.poutput(df.head())
                 self.poutput('\n')
             elif role == 'analyst':
-                data = {'values': [str(args.b)]}
-                response = requests.get(method + url_analyst + "/analyst_list_all_info_by_bu", auth=auth, json=data)
-                self.poutput("Here are the vulnerable computers in the business unit")
-                tabledata = response.json()
-                l1,l2 = len(tabledata), len(tabledata[0])
-                df = pd.DataFrame(tabledata, index=['']*l1, columns=['']*l2)
-                df.set_axis(['CVE', 'SEVERITY', 'COMPUTER NAME', 'OS', 'OS_VERSION', 'BUSINESS UNIT'],axis=1,inplace=True)
-                self.poutput(df.head())
-                self.poutput('\n')
+        # TODO: Enter the Vuln Count table here to see a list of all BUs with vulns
+                try:
+                    bus = input("Enter the Business Unit you'd like to see computers in: (leave blank for all): ")
+                    if bus == "":
+                        bus = "%"
+                    data = {'values': bus}
+                    response = requests.get(method + url_analyst + "/analyst_list_all_info_by_bu", auth=auth, json=data)
+                    tabledata = response.json()
+                    l1,l2 = len(tabledata), len(tabledata[0])
+                    df = pd.DataFrame(tabledata, index=['']*l1, columns=['']*l2)
+                    df.set_axis(['CVE', 'SEVERITY', 'COMPUTER NAME', 'OS', 'OS_VERSION', 'BUSINESS UNIT'],axis=1,inplace=True)
+                    self.poutput("Here are the vulnerable computers in the business unit")
+                    self.poutput(df.head())
+                    self.poutput('\n')
+                except IndexError:
+                    print("Please enter a valid Business Unit name.")
             elif role == 'engineer':
                 data = {'values': [str(args.b)]}
                 response = requests.get(method + url_engineer + "/engineer_access_bus", auth=auth, json=data)
@@ -265,6 +286,17 @@ class cvetracker(Cmd):
                     print("You do not have access to that business unit")
                 except IndexError:
                     print("The business unit you requested is empty.")
+            if role == 'analyst':
+                response = requests.get(method + url_analyst + "/analyst_vuln_count_per_bu", auth=auth)
+                tabledata = response.json()
+                l1,l2 = len(tabledata), len(tabledata[0])
+                df = pd.DataFrame(tabledata, index=['']*l1, columns=['']*l2)
+                df.set_axis(['BUSINESS UNIT', 'SEVERITY (>5.0)'],axis=1,inplace=True)
+                self.poutput("Here are the serious vulnerabilities in each business unit (severity > 5.0):")
+                self.poutput(df.head())
+                self.poutput('\n')
+        else:
+            self.poutput("You must log in to view this data.")
     parser_vuln.set_defaults(func=list_vulns)
 
     def list_apps(self, args):
@@ -351,6 +383,8 @@ class cvetracker(Cmd):
                     print("You do not have access to that business unit")
                 except IndexError:
                     print("The business unit you requested is empty.")
+        else:
+            self.poutput("You must log in to view this data.")
     parser_apps.set_defaults(func=list_apps)
 
 
